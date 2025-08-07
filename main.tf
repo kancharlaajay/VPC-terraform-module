dc9c9dd979e137047d939513b6287b414670070f
kancharlaajay <kancharlaajay1234@gmail.com>
2025-08-07 10:09:58 +0530

    new

--- main.tf
+++ main.tf
@@ -13,6 +13,13 @@ resource "aws_vpc" "main" {
   )
 }
 
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "default"
+  }
+}
+
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
